9f0e15b845f6704187e8267df882a4142671e658
Shambhavi Das <shambhavidas76@gmail.com>
2026-09-19 19:40:54 +0530

    Remove Week 2 files from Week 3 branch

--- src/week_2/assignment_problem/InventoryCsvParser.java
+++
@@ -1,8 +0,0 @@
-public class InventoryCsvParser {
-    public static void parseInventoryRecord(String csvLine) {
-        String[] fields = csvLine.split(",");
-        if (fields.length != 3) System.out.println("Invalid Record");
-        else System.out.println("Product: " + fields[0] + " | SKU: " + fields[1] + " | Qty: " + fields[2]);
-    }
-    public static void main(String[] args) { parseInventoryRecord("Wireless Mouse,WM-2201,150"); }
-}
